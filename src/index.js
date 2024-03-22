3ecdd56edfeb0feb826921ba5a2163a3a39c26c6
thanhvt2351 <84888879+thanhvt2351@users.noreply.github.com>
2024-03-22 18:29:07 +0700

    install express handle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
-  res.send('Hello World!')
+  res.send(`
+    <h1>Hello World!</h1>
+  `);
 })
 
 app.listen(port, () => {
